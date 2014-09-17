2d4c20e482eabdc722e373e63decd5fca53cf0f7
sbaldwin1 <sbaldwin1@student.rcc.edu>
2014-09-17 16:32:45 -0700

    Signed-off-by: sbaldwin1 <sbaldwin1@student.rcc.edu>

--- Hmwk/Assignment2/hwk.php
+++ Hmwk/Assignment2/hwk.php
@@ -14,42 +14,36 @@
 <body>
 <h1>The Electromagnetic Spectrum</h1>
 <?php
-	//Input the data from the form
-	$Frequency = $_GET['Frequency'];
-
+//Input the data from the form
+$Frequency = $_GET['Frequency'];
 ?>
-
-	<table width="200" border="1">
-		<tr>
-		<th>Radio</th>
-		<th>Microwave</th>
-		<th>Infared</th>
-		<th>Visiable</th>
-		<th>Ultraviolet</th>
-        <th>X-ray</th>
-        <th>Gamma Ray</th>
-		</tr>
+<table width="200" border="1">
+<tr>
+<th>Radio</th>
+<th>Microwave</th>
+<th>Infared</th>
+<th>Visiable</th>
+<th>Ultraviolet</th>
+<th>X-ray</th>
+<th>Gamma Ray</th>
+</tr>
 <?php
-
-		if($Frequency < pow(10,3)){
-			echo "Radio";
-		}else if($Frequency < pow(10,-2)){
-			echo "Microwave";
-		}else if ($Frequency < pow(10,-5)){
-			echo "Infared";
-		}else if ($Frequency < pow(10,-6)){
-			echo"Visiable";
-		}else if ($Frequency < pow(10,-8)){
-			echo "Ultraviolet";
-		}else if ($Frequency < pow(10,-10)){
-			echo "X-ray";
-		}else{
-			echo "Gamma Ray";
-		};
-		
-
+if($Frequency < pow(10,3)){
+echo "Radio";
+}else if($Frequency < pow(10,-2)){
+echo "Microwave";
+}else if ($Frequency < pow(10,-5)){
+echo "Infared";
+}else if ($Frequency < pow(10,-6)){
+echo"Visiable";
+}else if ($Frequency < pow(10,-8)){
+echo "Ultraviolet";
+}else if ($Frequency < pow(10,-10)){
+echo "X-ray";
+}else{
+echo "Gamma Ray";
+};
 ?>
-
 </table>
 </body>
 </html>
