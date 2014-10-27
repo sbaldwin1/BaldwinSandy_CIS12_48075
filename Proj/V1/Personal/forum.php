2dbb27d983b155316c3de3dbca8cce73112330f5
sbaldwin1 <sbaldwin1@student.rcc.edu>
2014-10-27 15:33:08 -0700

    Signed-off-by: sbaldwin1 <sbaldwin1@student.rcc.edu>

---
+++ Proj/V1/Personal/forum.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Forums</title>
+<?php
+/*
+this page is a forums. Whatever going on in forum and it will stay in there.
+*/
+?>
+</head>
+
+<body>
+</body>
+</html>
